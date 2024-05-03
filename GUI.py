ddac0d4331fc12b33a627f269cb37f635add08cc
Székely Áron <aron20010103@gmail.com>
2024-05-03 16:41:07 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,11 +1,15 @@
 import pygame
 pygame.init()
-
-
 pygame.display.set_caption("TorpedoAI")
+
+WIDTH = 1000
+HEIGHT = 600
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+GREY = (40,50,200)
 
 
 animating = True
+pausing = False
 while animating:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,3 +17,11 @@ while animating:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                animating = False
+
+            if event.type == pygame.K_SPACE:
+                pausing = not pausing
+    
+    if not pausing:
+        SCREEN.fill(GREY)
+        pygame.display.flip()
