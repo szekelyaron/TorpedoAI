18afee6ac29b0e32aea750000905019dfaa7ab06
Székely Áron <aron20010103@gmail.com>
2024-05-09 20:00:44 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,11 +2,24 @@ import pygame
 pygame.init()
 pygame.display.set_caption("TorpedoAI")
 
-WIDTH = 1000
-HEIGHT = 600
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
-GREY = (40,50,200)
+SQ_SIZE = 35
+H_MARGIN = SQ_SIZE * 4
+V_MARGIN = SQ_SIZE
 
+WIDTH = SQ_SIZE * 10 * 2 + H_MARGIN
+HEIGHT = SQ_SIZE * 10 * 2 + V_MARGIN
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+WHITE = (255,255,255)
+GREY = (40,50,60)
+RED = (255,40,0)
+BLUE = (0,0,128)
+
+def draw_grid(left = 0, top = 0):
+    for i in range(100) :
+        x = left + i % 10 * SQ_SIZE 
+        y = top + i // 10 * SQ_SIZE
+        square = pygame.Rect(x,y, SQ_SIZE,SQ_SIZE)
+        pygame.draw.rect(SCREEN, WHITE,square, width=3)
 
 animating = True
 pausing = False
@@ -24,4 +37,9 @@ while animating:
     
     if not pausing:
         SCREEN.fill(GREY)
+        draw_grid()
+        draw_grid(left = (WIDTH-H_MARGIN)//2 + H_MARGIN,top = (HEIGHT-V_MARGIN)//2 + V_MARGIN)
+
+        draw_grid(top = (HEIGHT-V_MARGIN)//2 + V_MARGIN)
+        draw_grid(left = (WIDTH-H_MARGIN)//2 + H_MARGIN)
         pygame.display.flip()
